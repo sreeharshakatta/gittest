15bae82f860d23b7df6465e20071537a2963107b
Sreeharsha Katta <katta.sr@northeastern.edu>
2021-12-02 13:15:02 -0500

    Backdating: 2021-12-02 13:15:02

---
+++ Assignment4/src/model/Community.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+
+public class Community {
+
+    public int getCommunityId() {
+        return communityId;
+    }
+
+    public void setCommunityId(int communityId) {
+        this.communityId = communityId;
+    }
+
+    public String getCommunityName() {
+        return communityName;
+    }
+
+    public void setCommunityName(String communityName) {
+        this.communityName = communityName;
+    }
+
+    public String getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(String houseId) {
+        this.houseId = houseId;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+      int communityId;
+      String communityName;
+      String houseId;
+      String city;
+    
+}
